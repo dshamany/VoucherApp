f5a2e2d93ad7813219ef1ac5f862a26bb642c889
devan <devanrterry@gmail.com>
2020-04-03 19:27:06 -0700

    complete scaffold

--- server.js
+++ server.js
@@ -5,5 +5,20 @@ const logger = require('morgan');
 
 const app = express();
 
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
+
 app.use(logger('dev'));
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+// api routes go here 
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
