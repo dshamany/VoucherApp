252e78d41fc0e0d9697f6111c8a295d0cb7c5a0c
Daniel Shamany <dshamany@gmail.com>
2020-04-09 18:09:40 -0700

    add navbar

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -5,12 +5,25 @@ import Landing from './Landing';
 import About from './About';
 import Signup from './Signup';
 
-function Index(props){
+function Index(props) {
     return (
-        <div className="main-container">
-            <Landing id="landing" />
-            <About id="about" />
-            <Signup id="signup"/>
+        <div>
+            <div id="navbar">
+                <div id="logo-container">
+                    <a href="#">
+                        <img id="logo" src="https://i.imgur.com/TpRJ9wg.png" alt="company logo"/>
+                    </a>
+                </div>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#signup">Sign Up</a></li>
+                </ul>
+            </div>
+            <div className="main-container">
+                <Landing id="landing" />
+                <About id="about" />
+                <Signup id="signup" />
+            </div>
         </div>
     )
 }
